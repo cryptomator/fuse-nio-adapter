6efa8c05bbfd970639b27eb53c09b3694b028fdd
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2017-12-15 13:53:15 +0100

    distinguish Mac and Linux mirroring test

--- src/test/java/org/cryptomator/frontend/fuse/MacMirroringTest.java
+++ src/test/java/org/cryptomator/frontend/fuse/MacMirroringTest.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
-public class PosixMirroringTest {
+public class MacMirroringTest {
 
 	public static void main(String[] args) throws IOException {
 		try (Scanner scanner = new Scanner(System.in)) {
