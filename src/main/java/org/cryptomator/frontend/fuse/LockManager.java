df5686bf50e306a3271020d751a36025e861b6d0
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2018-09-11 14:59:59 +0200

    Added DataLocks (preparation for #15)

--- src/main/java/org/cryptomator/frontend/fuse/LockManager.java
+++ src/main/java/org/cryptomator/frontend/fuse/LockManager.java
@@ -11,13 +11,34 @@ import javax.inject.Inject;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Provides a path-based locking mechanism as described by
  * <a href="https://people.eecs.berkeley.edu/~kubitron/courses/cs262a-F14/projects/reports/project6_report.pdf">Ritik Malhotra here</a>.
+ *
+ * <p>
+ * Usage Example 1:
+ * <pre>
+ *     try (PathLock pathLock = lockManager.lockPathForReading("/foo/bar/baz"); // path is not manipulated, thus read-locking
+ *          DataLock dataLock = pathLock.lockDataForWriting()) { // content is manipulated, thus write-locking
+ *          // write to file
+ *     }
+ * </pre>
+ *
+ * <p>
+ * Usage Example 2:
+ * <pre>
+ *     try (PathLock srcPathLock = lockManager.lockPathForReading("/foo/bar/original");
+ *          DataLock srcDataLock = srcPathLock.lockDataForReading(); // content will only be read, thus read-locking
+ *          PathLock dstPathLock = lockManager.lockPathForWriting("/foo/bar/copy"); // file will be created, thus write-locking
+ *          DataLock dstDataLock = srcPathLock.lockDataForWriting()) {
+ *          // copy from /foo/bar/original to /foo/bar/copy
+ *     }
+ * </pre>
  */
 @PerAdapter
 public class LockManager {
@@ -28,9 +49,11 @@ public class LockManager {
 	private static final Joiner PATH_JOINER = Joiner.on(PATH_SEP);
 
 	private final ConcurrentMap<String, ReentrantReadWriteLock> pathLocks = new ConcurrentHashMap<>();
+	private final ConcurrentMap<String, ReentrantReadWriteLock> dataLocks = new ConcurrentHashMap<>();
 
 	@Inject
-	public LockManager(){}
+	public LockManager() {
+	}
 
 	/*
 	 * Read locks:
@@ -46,16 +69,26 @@ public class LockManager {
 		List<String> parentPathComponents = parentPathComponents(pathComponents);
 		lockAncestors(parentPathComponents);
 		String path = PATH_JOINER.join(pathComponents);
-		getPathLock(path).readLock().lock();
-		return new PathLock(() -> unlockReadLock(pathComponents));
+		getLock(pathLocks, path).readLock().lock();
+		return new PathLock(path, this::unlockPathReadLock, this::lockDataForReading, this::lockDataForWriting);
 	}
 
-	private void unlockReadLock(List<String> pathComponents) {
+	private void unlockPathReadLock(String absolutePath) {
+		List<String> pathComponents = PATH_SPLITTER.splitToList(absolutePath);
 		List<String> parentPathComponents = parentPathComponents(pathComponents);
-		String path = PATH_JOINER.join(pathComponents);
-		getPathLock(path).readLock().unlock();
-		removeLockIfUnused(path);
+		getLock(pathLocks, absolutePath).readLock().unlock();
+		removeLockIfUnused(pathLocks, absolutePath);
 		unlockAncestors(parentPathComponents);
+	}
+
+	private DataLock lockDataForReading(String absolutePath) {
+		getLock(dataLocks, absolutePath).readLock().lock();
+		return new DataLock(absolutePath, this::unlockDataReadLock);
+	}
+
+	private void unlockDataReadLock(String absolutePath) {
+		getLock(dataLocks, absolutePath).readLock().unlock();
+		removeLockIfUnused(dataLocks, absolutePath);
 	}
 
 	/*
@@ -72,16 +105,26 @@ public class LockManager {
 		List<String> parentPathComponents = parentPathComponents(pathComponents);
 		lockAncestors(parentPathComponents);
 		String path = PATH_JOINER.join(pathComponents);
-		getPathLock(path).writeLock().lock();
-		return new PathLock(() -> unlockWriteLock(pathComponents));
+		getLock(pathLocks, path).writeLock().lock();
+		return new PathLock(path, this::unlockPathWriteLock, this::lockDataForReading, this::lockDataForWriting);
 	}
 
-	private void unlockWriteLock(List<String> pathComponents) {
+	private void unlockPathWriteLock(String absolutePath) {
+		List<String> pathComponents = PATH_SPLITTER.splitToList(absolutePath);
 		List<String> parentPathComponents = parentPathComponents(pathComponents);
-		String path = PATH_JOINER.join(pathComponents);
-		getPathLock(path).writeLock().unlock();
-		removeLockIfUnused(path);
+		getLock(pathLocks, absolutePath).writeLock().unlock();
+		removeLockIfUnused(pathLocks, absolutePath);
 		unlockAncestors(parentPathComponents);
+	}
+
+	private DataLock lockDataForWriting(String absolutePath) {
+		getLock(dataLocks, absolutePath).writeLock().lock();
+		return new DataLock(absolutePath, this::unlockDataWriteLock);
+	}
+
+	private void unlockDataWriteLock(String absolutePath) {
+		getLock(dataLocks, absolutePath).writeLock().unlock();
+		removeLockIfUnused(dataLocks, absolutePath);
 	}
 
 	/*
@@ -94,32 +137,32 @@ public class LockManager {
 			lockAncestors(parentPathComponents(pathComponents));
 		}
 		String path = PATH_JOINER.join(pathComponents);
-		getPathLock(path).readLock().lock();
+		getLock(pathLocks, path).readLock().lock();
 	}
 
 	// recusively release locks for children frist
 	private void unlockAncestors(List<String> pathComponents) {
 		String path = PATH_JOINER.join(pathComponents);
-		getPathLock(path).readLock().unlock();
-		removeLockIfUnused(path);
+		getLock(pathLocks, path).readLock().unlock();
+		removeLockIfUnused(pathLocks, path);
 		if (pathComponents.size() > 1) {
 			unlockAncestors(parentPathComponents(pathComponents));
 		}
 	}
 
-	private ReadWriteLock getPathLock(String path) {
-		return pathLocks.computeIfAbsent(path, p -> {
-			LOG.trace("Creating Lock for {}", path);
+	private ReadWriteLock getLock(ConcurrentMap<String, ReentrantReadWriteLock> map, String path) {
+		return map.computeIfAbsent(path, p -> {
+			LOG.trace("Creating Lock for {}", p);
 			return new ReentrantReadWriteLock(true);
 		});
 	}
 
-	private void removeLockIfUnused(String path) {
-		pathLocks.compute(path, (p, l) -> {
+	private void removeLockIfUnused(ConcurrentMap<String, ReentrantReadWriteLock> map, String path) {
+		map.compute(path, (p, l) -> {
 			if (l.writeLock().tryLock()) { // if we can become the exlusive lock holder
 				try {
 					if (!l.hasQueuedThreads()) { // and if nobody else is waiting for a lock
-						LOG.trace("Removing Lock for {}", path);
+						LOG.trace("Removing Lock for {}", p);
 						return null; // then remove this map entry
 					}
 				} finally {
@@ -131,7 +174,7 @@ public class LockManager {
 	}
 
 	// visible for testing
-	boolean isLocked(String absolutePath) {
+	boolean isPathLocked(String absolutePath) {
 		String relativePath = CharMatcher.is(PATH_SEP).trimFrom(absolutePath);
 		return pathLocks.containsKey(relativePath);
 	}
@@ -142,15 +185,45 @@ public class LockManager {
 
 	public static class PathLock implements AutoCloseable {
 
-		private final Runnable unlockFunction;
+		private final String path;
+		private final Consumer<String> unlockFn;
+		private final Function<String, DataLock> readDataLockFn;
+		private final Function<String, DataLock> writeDataLockFn;
 
-		private PathLock(Runnable unlockFunction) {
-			this.unlockFunction = unlockFunction;
+		private PathLock(String path, Consumer<String> unlockFn, Function<String, DataLock> readDataLockFn, Function<String, DataLock> writeDataLockFn) {
+			this.path = path;
+			this.unlockFn = unlockFn;
+			this.readDataLockFn = readDataLockFn;
+			this.writeDataLockFn = writeDataLockFn;
+		}
+
+		public DataLock lockDataForReading() {
+			return readDataLockFn.apply(path);
+		}
+
+		public DataLock lockDataForWriting() {
+			return writeDataLockFn.apply(path);
 		}
 
 		@Override
 		public void close() {
-			unlockFunction.run();
+			unlockFn.accept(path);
+		}
+	}
+
+	public static class DataLock implements AutoCloseable {
+
+		private final String path;
+		private final Consumer<String> unlockFn;
+
+		private DataLock(String path, Consumer<String> unlockFn) {
+			this.path = path;
+			this.unlockFn = unlockFn;
+		}
+
+		@Override
+		public void close() {
+			unlockFn.accept(path);
 		}
 	}
 
--- src/test/java/org/cryptomator/frontend/fuse/LockManagerTest.java
+++ src/test/java/org/cryptomator/frontend/fuse/LockManagerTest.java
@@ -1,7 +1,10 @@
 package org.cryptomator.frontend.fuse;
 
+import org.cryptomator.frontend.fuse.LockManager.DataLock;
 import org.cryptomator.frontend.fuse.LockManager.PathLock;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,96 +24,170 @@ public class LockManagerTest {
 
 	private static final Logger LOG = LoggerFactory.getLogger(LockManagerTest.class);
 
-	@Test
-	public void testLockCountDuringLock() {
-		LockManager lockManager = new LockManager();
-		Assertions.assertFalse(lockManager.isLocked("/foo"));
-		Assertions.assertFalse(lockManager.isLocked("/foo/bar"));
-		Assertions.assertFalse(lockManager.isLocked("/foo/bar/baz"));
-		try (PathLock lock1 = lockManager.lockPathForReading("/foo/bar/baz")) {
-			Assertions.assertTrue(lockManager.isLocked("/foo"));
-			Assertions.assertTrue(lockManager.isLocked("/foo/bar"));
-			Assertions.assertTrue(lockManager.isLocked("/foo/bar/baz"));
-			try (PathLock lock2 = lockManager.lockPathForReading("/foo/bar/baz")) {
-				Assertions.assertTrue(lockManager.isLocked("/foo"));
-				Assertions.assertTrue(lockManager.isLocked("/foo/bar"));
-				Assertions.assertTrue(lockManager.isLocked("/foo/bar/baz"));
+	@Nested
+	@DisplayName("PathLocks")
+	class PathLockTests {
+
+		@Test
+		public void testLockCountDuringLock() {
+			LockManager lockManager = new LockManager();
+			Assertions.assertFalse(lockManager.isPathLocked("/foo"));
+			Assertions.assertFalse(lockManager.isPathLocked("/foo/bar"));
+			Assertions.assertFalse(lockManager.isPathLocked("/foo/bar/baz"));
+			try (PathLock lock1 = lockManager.lockPathForReading("/foo/bar/baz")) {
+				Assertions.assertTrue(lockManager.isPathLocked("/foo"));
+				Assertions.assertTrue(lockManager.isPathLocked("/foo/bar"));
+				Assertions.assertTrue(lockManager.isPathLocked("/foo/bar/baz"));
+				try (PathLock lock2 = lockManager.lockPathForReading("/foo/bar/baz")) {
+					Assertions.assertTrue(lockManager.isPathLocked("/foo"));
+					Assertions.assertTrue(lockManager.isPathLocked("/foo/bar"));
+					Assertions.assertTrue(lockManager.isPathLocked("/foo/bar/baz"));
+				}
+				Assertions.assertTrue(lockManager.isPathLocked("/foo"));
+				Assertions.assertTrue(lockManager.isPathLocked("/foo/bar"));
+				Assertions.assertTrue(lockManager.isPathLocked("/foo/bar/baz"));
+				try (PathLock lock3 = lockManager.lockPathForReading("/foo/bar/baz")) {
+					Assertions.assertTrue(lockManager.isPathLocked("/foo"));
+					Assertions.assertTrue(lockManager.isPathLocked("/foo/bar"));
+					Assertions.assertTrue(lockManager.isPathLocked("/foo/bar/baz"));
+				}
+				Assertions.assertTrue(lockManager.isPathLocked("/foo"));
+				Assertions.assertTrue(lockManager.isPathLocked("/foo/bar"));
+				Assertions.assertTrue(lockManager.isPathLocked("/foo/bar/baz"));
 			}
-			Assertions.assertTrue(lockManager.isLocked("/foo"));
-			Assertions.assertTrue(lockManager.isLocked("/foo/bar"));
-			Assertions.assertTrue(lockManager.isLocked("/foo/bar/baz"));
-			try (PathLock lock3 = lockManager.lockPathForReading("/foo/bar/baz")) {
-				Assertions.assertTrue(lockManager.isLocked("/foo"));
-				Assertions.assertTrue(lockManager.isLocked("/foo/bar"));
-				Assertions.assertTrue(lockManager.isLocked("/foo/bar/baz"));
-			}
-			Assertions.assertTrue(lockManager.isLocked("/foo"));
-			Assertions.assertTrue(lockManager.isLocked("/foo/bar"));
-			Assertions.assertTrue(lockManager.isLocked("/foo/bar/baz"));
+			Assertions.assertFalse(lockManager.isPathLocked("/foo"));
+			Assertions.assertFalse(lockManager.isPathLocked("/foo/bar"));
+			Assertions.assertFalse(lockManager.isPathLocked("/foo/bar/baz"));
 		}
-		Assertions.assertFalse(lockManager.isLocked("/foo"));
-		Assertions.assertFalse(lockManager.isLocked("/foo/bar"));
-		Assertions.assertFalse(lockManager.isLocked("/foo/bar/baz"));
+
+		@Test
+		public void testMultipleReadLocks() throws InterruptedException {
+			LockManager lockManager = new LockManager();
+			int numThreads = 8;
+			ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
+			CountDownLatch done = new CountDownLatch(numThreads);
+			AtomicInteger counter = new AtomicInteger();
+			AtomicInteger maxCounter = new AtomicInteger();
+
+			for (int i = 0; i < numThreads; i++) {
+				int threadnum = i;
+				threadPool.submit(() -> {
+					try (PathLock lock = lockManager.lockPathForReading("/foo/bar/baz")) {
+						LOG.debug("ENTER thread {}", threadnum);
+						counter.incrementAndGet();
+						Thread.sleep(50);
+						maxCounter.set(Math.max(counter.get(), maxCounter.get()));
+						counter.decrementAndGet();
+						LOG.debug("LEAVE thread {}", threadnum);
+					} catch (InterruptedException e) {
+						LOG.error("thread interrupted", e);
+					}
+					done.countDown();
+				});
+			}
+
+			done.await();
+			Assertions.assertEquals(numThreads, maxCounter.get());
+		}
+
+		@Test
+		public void testMultipleWriteLocks() throws InterruptedException {
+			LockManager lockManager = new LockManager();
+			int numThreads = 8;
+			ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
+			CountDownLatch done = new CountDownLatch(numThreads);
+			AtomicInteger counter = new AtomicInteger();
+			AtomicInteger maxCounter = new AtomicInteger();
+
+			for (int i = 0; i < numThreads; i++) {
+				int threadnum = i;
+				threadPool.submit(() -> {
+					try (PathLock lock = lockManager.lockPathForWriting("/foo/bar/baz")) {
+						LOG.debug("ENTER thread {}", threadnum);
+						counter.incrementAndGet();
+						Thread.sleep(10);
+						maxCounter.set(Math.max(counter.get(), maxCounter.get()));
+						counter.decrementAndGet();
+						LOG.debug("LEAVE thread {}", threadnum);
+					} catch (InterruptedException e) {
+						LOG.error("thread interrupted", e);
+					}
+					done.countDown();
+				});
+			}
+
+			done.await();
+			Assertions.assertEquals(1, maxCounter.get());
+		}
+
 	}
 
-	@Test
-	public void testMultipleReadLocks() throws InterruptedException {
-		LockManager lockManager = new LockManager();
-		int numThreads = 8;
-		ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
-		CountDownLatch done = new CountDownLatch(numThreads);
-		AtomicInteger counter = new AtomicInteger();
-		AtomicInteger maxCounter = new AtomicInteger();
+	@Nested
+	@DisplayName("DataLocks")
+	class DataLockTests {
 
-		for (int i = 0; i < numThreads; i++) {
-			int threadnum = i;
-			threadPool.submit(() -> {
-				try (PathLock lock = lockManager.lockPathForReading("/foo/bar/baz")) {
-					LOG.debug("ENTER thread {}", threadnum);
-					counter.incrementAndGet();
-					Thread.sleep(50);
-					maxCounter.set(Math.max(counter.get(), maxCounter.get()));
-					counter.decrementAndGet();
-					LOG.debug("LEAVE thread {}", threadnum);
-				} catch (InterruptedException e) {
-					LOG.error("thread interrupted", e);
-				}
-				done.countDown();
-			});
+		@Test
+		public void testMultipleReadLocks() throws InterruptedException {
+			LockManager lockManager = new LockManager();
+			int numThreads = 8;
+			ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
+			CountDownLatch done = new CountDownLatch(numThreads);
+			AtomicInteger counter = new AtomicInteger();
+			AtomicInteger maxCounter = new AtomicInteger();
+
+			for (int i = 0; i < numThreads; i++) {
+				int threadnum = i;
+				threadPool.submit(() -> {
+					try (PathLock pathLock = lockManager.lockPathForReading("/foo/bar/baz"); //
+						 DataLock dataLock = pathLock.lockDataForReading()) {
+						LOG.debug("ENTER thread {}", threadnum);
+						counter.incrementAndGet();
+						Thread.sleep(50);
+						maxCounter.set(Math.max(counter.get(), maxCounter.get()));
+						counter.decrementAndGet();
+						LOG.debug("LEAVE thread {}", threadnum);
+					} catch (InterruptedException e) {
+						LOG.error("thread interrupted", e);
+					}
+					done.countDown();
+				});
+			}
+
+			done.await();
+			Assertions.assertEquals(numThreads, maxCounter.get());
 		}
 
-		done.await();
-		Assertions.assertEquals(numThreads, maxCounter.get());
-	}
+		@Test
+		public void testMultipleWriteLocks() throws InterruptedException {
+			LockManager lockManager = new LockManager();
+			int numThreads = 8;
+			ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
+			CountDownLatch done = new CountDownLatch(numThreads);
+			AtomicInteger counter = new AtomicInteger();
+			AtomicInteger maxCounter = new AtomicInteger();
 
-	@Test
-	public void testMultipleWriteLocks() throws InterruptedException {
-		LockManager lockManager = new LockManager();
-		int numThreads = 8;
-		ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
-		CountDownLatch done = new CountDownLatch(numThreads);
-		AtomicInteger counter = new AtomicInteger();
-		AtomicInteger maxCounter = new AtomicInteger();
+			for (int i = 0; i < numThreads; i++) {
+				int threadnum = i;
+				threadPool.submit(() -> {
+					try (PathLock pathLock = lockManager.lockPathForReading("/foo/bar/baz"); //
+						 DataLock dataLock = pathLock.lockDataForWriting()) {
+						LOG.debug("ENTER thread {}", threadnum);
+						counter.incrementAndGet();
+						Thread.sleep(10);
+						maxCounter.set(Math.max(counter.get(), maxCounter.get()));
+						counter.decrementAndGet();
+						LOG.debug("LEAVE thread {}", threadnum);
+					} catch (InterruptedException e) {
+						LOG.error("thread interrupted", e);
+					}
+					done.countDown();
+				});
+			}
 
-		for (int i = 0; i < numThreads; i++) {
-			int threadnum = i;
-			threadPool.submit(() -> {
-				try (PathLock lock = lockManager.lockPathForWriting("/foo/bar/baz")) {
-					LOG.debug("ENTER thread {}", threadnum);
-					counter.incrementAndGet();
-					Thread.sleep(10);
-					maxCounter.set(Math.max(counter.get(), maxCounter.get()));
-					counter.decrementAndGet();
-					LOG.debug("LEAVE thread {}", threadnum);
-				} catch (InterruptedException e) {
-					LOG.error("thread interrupted", e);
-				}
-				done.countDown();
-			});
+			done.await();
+			Assertions.assertEquals(1, maxCounter.get());
 		}
 
-		done.await();
-		Assertions.assertEquals(1, maxCounter.get());
 	}
 
 }
