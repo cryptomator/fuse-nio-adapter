344a41cea5392de38e9d618bb66f54b553cfce21
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2019-04-12 11:49:34 +0200

    Added several variations of the mirroring test, including cryptofs

--- src/test/java/org/cryptomator/frontend/fuse/mount/MirroringFuseMountTest.java
+++ src/test/java/org/cryptomator/frontend/fuse/mount/MirroringFuseMountTest.java
@@ -1,14 +1,19 @@
 package org.cryptomator.frontend.fuse.mount;
 
+import com.google.common.base.Preconditions;
+import org.cryptomator.cryptofs.CryptoFileSystemProperties;
+import org.cryptomator.cryptofs.CryptoFileSystemProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.impl.SimpleLogger;
 
 import java.io.IOException;
+import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class MirroringFuseMountTest {
 
@@ -19,21 +24,104 @@ public class MirroringFuseMountTest {
 	}
 
 	private static final Logger LOG = LoggerFactory.getLogger(MirroringFuseMountTest.class);
+	private static final String OS_NAME = System.getProperty("os.name").toLowerCase();
 
-	public static void main(String[] args) {
-		try (Scanner scanner = new Scanner(System.in)) {
-			System.out.println("Enter path to the directory you want to mirror:");
-			Path p = Paths.get(scanner.nextLine());
-			System.out.println("Enter mount point:");
-			Path m = Paths.get(scanner.nextLine());
-			if (m.startsWith(p) || p.startsWith(m)) {
-				System.err.println("Mirrored directory and mount location must not be nested.");
-			} else if (Files.isDirectory(p)) {
-				mount(p, m);
-			} else {
-				LOG.error("Invalid directory.");
+	/**
+	 * Mirror directory on Linux
+	 */
+	public static class LinuxMirror {
+
+		public static void main(String args[]) {
+			Preconditions.checkState(OS_NAME.contains("linux"), "Test designed to run on Linux.");
+
+			try (Scanner scanner = new Scanner(System.in)) {
+				System.out.println("Enter path to the directory you want to mirror:");
+				Path p = Paths.get(scanner.nextLine());
+				System.out.println("Enter mount point:");
+				Path m = Paths.get(scanner.nextLine());
+				if (m.startsWith(p) || p.startsWith(m)) {
+					LOG.error("Mirrored directory and mount location must not be nested.");
+				} else if (Files.isDirectory(p)) {
+					mount(p, m);
+				} else {
+					LOG.error("Invalid directory.");
+				}
 			}
 		}
+
+	}
+
+	/**
+	 * Mirror vault on Linux
+	 */
+	public static class LinuxCryptoFsMirror {
+
+		public static void main(String args[]) throws IOException {
+			Preconditions.checkState(OS_NAME.contains("linux"), "Test designed to run on Linux.");
+
+			try (Scanner scanner = new Scanner(System.in)) {
+				System.out.println("Enter path to the vault you want to mirror:");
+				Path vaultPath = Paths.get(scanner.nextLine());
+				Preconditions.checkArgument(CryptoFileSystemProvider.containsVault(vaultPath, "masterkey.cryptomator"), "Not a vault: " + vaultPath);
+				System.out.println("Enter vault password:");
+				String passphrase = scanner.nextLine();
+				CryptoFileSystemProperties props = CryptoFileSystemProperties.withPassphrase(passphrase).withFlags().build();
+				try (FileSystem cryptoFs = CryptoFileSystemProvider.newFileSystem(vaultPath, props)) {
+					Path p = cryptoFs.getPath("/");
+					System.out.println("Enter mount point:");
+					Path m = Paths.get(scanner.nextLine());
+					Preconditions.checkArgument(Files.isDirectory(m), "Invalid mount point: " + m);
+					LOG.info("Mounting FUSE file system at {}", m);
+					mount(p, m);
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Mirror directory on macOS
+	 */
+	public static class MacMirror {
+
+		public static void main(String args[]) {
+			Preconditions.checkState(OS_NAME.contains("mac"), "Test designed to run on macOS.");
+
+			try (Scanner scanner = new Scanner(System.in)) {
+				System.out.println("Enter path to the directory you want to mirror:");
+				Path p = Paths.get(scanner.nextLine());
+				Path m = Paths.get("/Volumes/" + UUID.randomUUID().toString());
+				LOG.info("Mounting FUSE file system at {}", m);
+				mount(p, m);
+			}
+		}
+
+	}
+
+	/**
+	 * Mirror vault on macOS
+	 */
+	public static class MacCryptoFsMirror {
+
+		public static void main(String args[]) throws IOException {
+			Preconditions.checkState(OS_NAME.contains("mac"), "Test designed to run on macOS.");
+
+			try (Scanner scanner = new Scanner(System.in)) {
+				System.out.println("Enter path to the vault you want to mirror:");
+				Path vaultPath = Paths.get(scanner.nextLine());
+				Preconditions.checkArgument(CryptoFileSystemProvider.containsVault(vaultPath, "masterkey.cryptomator"), "Not a vault: " + vaultPath);
+				System.out.println("Enter vault password:");
+				String passphrase = scanner.nextLine();
+				CryptoFileSystemProperties props = CryptoFileSystemProperties.withPassphrase(passphrase).withFlags().build();
+				try (FileSystem cryptoFs = CryptoFileSystemProvider.newFileSystem(vaultPath, props)) {
+					Path p = cryptoFs.getPath("/");
+					Path m = Paths.get("/Volumes/" + UUID.randomUUID().toString());
+					LOG.info("Mounting FUSE file system at {}", m);
+					mount(p, m);
+				}
+			}
+		}
+
 	}
 
 	private static void mount(Path pathToMirror, Path mountPoint) {
