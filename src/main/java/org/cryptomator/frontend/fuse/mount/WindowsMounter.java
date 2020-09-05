b7864a2b1ec9c21c4c126c9d0ec207cdad557a49
JaniruTEC <52893617+JaniruTEC@users.noreply.github.com>
2020-09-05 21:09:58 +0200

    Added check for WinFsp installation to WindowsMounter

--- src/main/java/org/cryptomator/frontend/fuse/mount/WindowsMounter.java
+++ src/main/java/org/cryptomator/frontend/fuse/mount/WindowsMounter.java
@@ -3,12 +3,17 @@ package org.cryptomator.frontend.fuse.mount;
 import jnr.ffi.Platform;
 import org.cryptomator.frontend.fuse.AdapterFactory;
 import org.cryptomator.frontend.fuse.FuseNioAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.serce.jnrfuse.FuseException;
+import ru.serce.jnrfuse.utils.WinPathUtils;
 
 import java.nio.file.Path;
 
 class WindowsMounter implements Mounter {
 
 	private static final boolean IS_WINDOWS = Platform.getNativePlatform().getOS() == Platform.OS.WINDOWS;
+	private static final Logger LOG = LoggerFactory.getLogger(WindowsMounter.class);
 
 	@Override
 	public synchronized Mount mount(Path directory, boolean blocking, boolean debug, EnvironmentVariables envVars) throws CommandFailedException {
@@ -28,7 +33,18 @@ class WindowsMounter implements Mounter {
 
 	@Override
 	public boolean isApplicable() {
-		return IS_WINDOWS;
+		return IS_WINDOWS && isWinFspInstalled();
+	}
+
+	private boolean isWinFspInstalled() {
+		try {
+			String path = WinPathUtils.getWinFspPath(); //Result only matters for debug-message; null-check is included in lib
+			LOG.trace("Found WinFsp installation at {}", path);
+			return true;
+		} catch(FuseException exc) {
+			LOG.debug("Failed to find a WinFsp installation; that's only a problem if you want to use FUSE on Windows. Exception text: \"{}\"", exc.getMessage());
+			return false;
+		}
 	}
 
 	private static class WindowsMount extends AbstractMount {
