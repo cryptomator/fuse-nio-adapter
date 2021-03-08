112f58cafb3238f8f7c559901a086c4c3f3f7805
Armin Schrenk <armin.schrenk@skymatic.de>
2021-03-08 17:19:59 +0100

    add documentation

--- src/main/java/org/cryptomator/frontend/fuse/AdapterFactory.java
+++ src/main/java/org/cryptomator/frontend/fuse/AdapterFactory.java
@@ -9,6 +9,13 @@ public class AdapterFactory {
 	private AdapterFactory() {
 	}
 
+	/**
+	 * Creates a read-only fuse-nio filesystem with a maximum file name length of {@value DEFAULT_NAME_MAX} and an assumed filename encoding of UTF-8 NFC for FUSE and the NIO filesystem.
+	 * @param root the root path of the NIO filesystem.
+	 * @return an adapter mapping FUSE callbacks to the nio interface
+	 * @see ReadOnlyAdapter
+	 * @see FileNameTranscoder
+	 */
 	public static FuseNioAdapter createReadOnlyAdapter(Path root) {
 		return createReadOnlyAdapter(root, DEFAULT_NAME_MAX, FileNameTranscoder.transcoder() );
 	}
@@ -22,10 +29,24 @@ public class AdapterFactory {
 		return comp.readOnlyAdapter();
 	}
 
+	/**
+	 * Creates a fuse-nio-filesystem with a maximum file name length of {@value DEFAULT_NAME_MAX} and an assumed filename encoding of UTF-8 NFC for FUSE and the NIO filesystem.
+	 * @param root the root path of the NIO filesystem.
+	 * @return an adapter mapping FUSE callbacks to the nio interface
+	 * @see ReadWriteAdapter
+	 * @see FileNameTranscoder
+	 */
 	public static FuseNioAdapter createReadWriteAdapter(Path root) {
 		return createReadWriteAdapter(root, DEFAULT_NAME_MAX);
 	}
 
+	/**
+	 * Creates a fuse-nio-filesystem with an assumed filename encoding of UTF-8 NFC for FUSE and the NIO filesystem.
+	 * @param root the root path of the NIO filesystem.
+	 * @return an adapter mapping FUSE callbacks to the nio interface
+	 * @see ReadWriteAdapter
+	 * @see FileNameTranscoder
+	 */
 	public static FuseNioAdapter createReadWriteAdapter(Path root, int maxFileNameLength) {
 		FuseNioAdapterComponent comp = DaggerFuseNioAdapterComponent.builder().root(root).maxFileNameLength(maxFileNameLength).fileNameTranscoder(FileNameTranscoder.transcoder()).build();
 		return comp.readWriteAdapter();
