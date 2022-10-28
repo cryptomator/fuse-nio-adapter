b9595dbfe7fd3c72082500331c6628ae0e3a1900
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2022-10-28 15:41:07 +0200

    Revert 5bd5eb5: Apparently inspecting annotations is not considered to be a reflective operation.
    See https://stackoverflow.com/a/69514065/4014509

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,7 +13,4 @@ module org.cryptomator.frontend.fuse {
 	requires static org.jetbrains.annotations;
 
 	provides MountProvider with LinuxFuseProvider, MacFuseMountProvider, FuseTMountProvider, WinFspMountProvider;
-
-	// integrations-api needs reflective access to check annotations on the mount providers:
-	opens org.cryptomator.frontend.fuse.mount to org.cryptomator.integrations.api;
 }
