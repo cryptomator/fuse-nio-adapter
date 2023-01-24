ce015899dae0cf41e3810c05682e27873656e410
Tobias Hagemann <tobias.hagemann@skymatic.de>
2023-01-24 16:01:23 +0100

    FUSE-T: Removing VOLUME_NAME capability because it breaks statfs()

--- src/main/java/org/cryptomator/frontend/fuse/mount/FuseTMountProvider.java
+++ src/main/java/org/cryptomator/frontend/fuse/mount/FuseTMountProvider.java
@@ -21,12 +21,10 @@ import java.text.Normalizer;
 import java.util.EnumSet;
 import java.util.Set;
 
-import static org.cryptomator.integrations.mount.MountCapability.LOOPBACK_PORT;
 import static org.cryptomator.integrations.mount.MountCapability.MOUNT_FLAGS;
 import static org.cryptomator.integrations.mount.MountCapability.MOUNT_TO_EXISTING_DIR;
 import static org.cryptomator.integrations.mount.MountCapability.READ_ONLY;
 import static org.cryptomator.integrations.mount.MountCapability.UNMOUNT_FORCED;
-import static org.cryptomator.integrations.mount.MountCapability.VOLUME_NAME;
 
 /**
  * Mounts a file system on macOS using fuse-t.
@@ -41,7 +39,7 @@ public class FuseTMountProvider implements MountService {
 
 	@Override
 	public String displayName() {
-		return "FUSE-T";
+		return "FUSE-T (Experimental)";
 	}
 
 	@Override
@@ -56,7 +54,7 @@ public class FuseTMountProvider implements MountService {
 
 	@Override
 	public Set<MountCapability> capabilities() {
-		return EnumSet.of(MOUNT_FLAGS, UNMOUNT_FORCED, READ_ONLY, MOUNT_TO_EXISTING_DIR, VOLUME_NAME); // adjusting LOOPBACK_PORT is currently broken
+		return EnumSet.of(MOUNT_FLAGS, UNMOUNT_FORCED, READ_ONLY, MOUNT_TO_EXISTING_DIR); // LOOPBACK_PORT and VOLUME_NAME are currently broken
 	}
 
 	@Override
