a4241993298e192bdfd6c86beedbfe871d2f5c94
Armin Schrenk <armin.schrenk@skymatic.de>
2023-11-21 12:17:08 +0100

    references #86

--- src/main/java/org/cryptomator/frontend/fuse/mount/WinFspMountProvider.java
+++ src/main/java/org/cryptomator/frontend/fuse/mount/WinFspMountProvider.java
@@ -119,7 +119,8 @@ public class WinFspMountProvider implements MountService {
 			var builder = Fuse.builder();
 			var libPath = WinfspUtil.getWinFspInstallDir() + "bin\\" + (OS_ARCH.contains("aarch64") ? "winfsp-a64.dll" : "winfsp-x64.dll");
 			builder.setLibraryPath(libPath);
-			var fuseAdapter = ReadWriteAdapter.create(builder.errno(), vfsRoot, FuseNioAdapter.DEFAULT_MAX_FILENAMELENGTH, FileNameTranscoder.transcoder(), true);
+			//xattr disabled due to https://github.com/cryptomator/fuse-nio-adapter/issues/86
+			var fuseAdapter = ReadWriteAdapter.create(builder.errno(), vfsRoot, FuseNioAdapter.DEFAULT_MAX_FILENAMELENGTH, FileNameTranscoder.transcoder(), false);
 			try {
 				var fuse = builder.build(fuseAdapter);
 				fuse.mount("fuse-nio-adapter", mountPoint, combinedMountFlags().toArray(String[]::new));
